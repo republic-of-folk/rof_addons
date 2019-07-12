76d1c43c469dc28303ae656d753e2149632f341a
boberro <m.cyraniak@gmail.com>
2019-07-12 21:51:07 +0200

    rof_patch_us_military_units - backpacks

--- rof_patch_us_military_units/backpacks.hpp
+++ rof_patch_us_military_units/backpacks.hpp
@@ -1,146 +1,144 @@
-    class Bag_Base;
-    class usm_pack_alice;
-    class usm_pack_alice_prc119: Bag_Base
+class Bag_Base;
+class usm_pack_alice;
+class usm_pack_alice_prc119: Bag_Base
+{
+    scope = 2;
+    maximumLoad = 180;
+    mass = 60;
+    class TransportMagazines
     {
-        scope = 2;
-        maximumLoad = 180;
-        mass = 60;
-        class TransportMagazines
+        delete _xx_SmokeShellGreen;
+        delete _xx_SmokeShellRed;
+        delete _xx_SmokeShellYellow;
+    };
+    class TransportItems
+    {
+        class _xx_ACRE_PRC117F
         {
-            delete _xx_SmokeShellGreen;
-            delete _xx_SmokeShellRed;
-            delete _xx_SmokeShellYellow;
-        };
-        class TransportItems
-        {
-            class _xx_ACRE_PRC117F
-            {
-                name = "ACRE_PRC117F";
-                count = 1;
-            };
+            name = "ACRE_PRC117F";
+            count = 1;
         };
     };
-    class usm_pack_alice_prc77: Bag_Base
+};
+class usm_pack_alice_prc77: Bag_Base
+{
+    maximumLoad = 180;
+    mass = 60;
+    class TransportMagazines
     {
-        maximumLoad = 180;
-        mass = 60;
-        class TransportMagazines
+        delete _xx_SmokeShellGreen;
+        delete _xx_SmokeShellRed;
+        delete _xx_SmokeShellYellow;
+    };
+    class TransportItems
+    {
+        class _xx_ACRE_PRC77
         {
-            delete _xx_SmokeShellGreen;
-            delete _xx_SmokeShellRed;
-            delete _xx_SmokeShellYellow;
-        };
-        class TransportItems
-        {
-            class _xx_ACRE_PRC77
-            {
-                name = "ACRE_PRC77";
-                count = 1;
-            };
+            name = "ACRE_PRC77";
+            count = 1;
         };
     };
-    class usm_pack_st138_prc77: Bag_Base
+};
+class usm_pack_st138_prc77: Bag_Base
+{
+    maximumLoad = 120;
+    mass = 20;
+    class TransportMagazines
     {
-        maximumLoad = 120;
-        mass = 20;
-        class TransportMagazines
+        delete _xx_SmokeShellGreen;
+        delete _xx_SmokeShellRed;
+        delete _xx_SmokeShellYellow;
+    };
+    class TransportItems
+    {
+        class _xx_ACRE_PRC77
         {
-            delete _xx_SmokeShellGreen;
-            delete _xx_SmokeShellRed;
-            delete _xx_SmokeShellYellow;
-        };
-        class TransportItems
-        {
-            class _xx_ACRE_PRC77
-            {
-                name = "ACRE_PRC77";
-                count = 1;
-            };
+            name = "ACRE_PRC77";
+            count = 1;
         };
     };
-    class usm_pack_alice_medic: usm_pack_alice
+};
+class usm_pack_alice_medic: usm_pack_alice
+{
+    class TransportMagazines
     {
-        class TransportMagazines
+        delete _xx_SmokeShellPurple;
+        delete _xx_SmokeShellYellow;
+    };
+    class TransportItems
+    {
+        delete _xx_usm_fielddressing;
+        delete _xx_usm_medikit;
+        class _xx_FirstAidKit
         {
-            delete _xx_SmokeShellPurple;
-            delete _xx_SmokeShellYellow;
+            name = "FirstAidKit";
+            count = 4;
         };
-        class TransportItems
+        class _xx_Medikit
         {
-            delete _xx_usm_fielddressing;
-            delete _xx_usm_medikit;
-            class _xx_FirstAidKit
-            {
-                name = "FirstAidKit";
-                count = 4;
-            };
-            class _xx_Medikit
-            {
-                name = "Medikit";
-                count = 1;
-            };
+            name = "Medikit";
+            count = 1;
         };
     };
-    class usm_pack_m5_medic: Bag_Base
+};
+class usm_pack_m5_medic: Bag_Base
+{
+    class TransportMagazines
     {
-        class TransportMagazines
-        {
-            delete _xx_SmokeShellPurple;
-            delete _xx_SmokeShellYellow;
-        };
-        class TransportItems
-        {
-            delete _xx_usm_fielddressing;
-            delete _xx_usm_medikit;
-            class _xx_FirstAidKit
-            {
-                name = "FirstAidKit";
-                count = 4;
-            };
-            class _xx_Medikit
-            {
-                name = "Medikit";
-                count = 1;
-            };
-        };
+        delete _xx_SmokeShellPurple;
+        delete _xx_SmokeShellYellow;
     };
-    class usm_pack_762x51_bandoliers: Bag_Base
+    class TransportItems
     {
-        class TransportMagazines
+        delete _xx_usm_fielddressing;
+        delete _xx_usm_medikit;
+        class _xx_FirstAidKit
         {
-            delete _xx_CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M;
-            class _xx_rhsusf_100Rnd_762x51
-            {
-                magazine = "rhsusf_100Rnd_762x51";
-                count = 3;
-            };
+            name = "FirstAidKit";
+            count = 4;
+        };
+        class _xx_Medikit
+        {
+            name = "Medikit";
+            count = 1;
         };
     };
-    class usm_pack_200rnd_556_bandoliers: Bag_Base
+};
+class usm_pack_762x51_bandoliers: Bag_Base
+{
+    class TransportMagazines
     {
-        class TransportMagazines
+        delete _xx_CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M;
+        class _xx_rhsusf_100Rnd_762x51
         {
-            delete _xx_CUP_200Rnd_TE4_Red_Tracer_556x45_M249;
-            class _xx_rhs_200rnd_556x45_M_SAW
-            {
-                magazine = "rhs_200rnd_556x45_M_SAW";
-                count = 3;
-            };
+            magazine = "rhsusf_100Rnd_762x51";
+            count = 3;
         };
     };
-    class usm_pack_762x51_ammobelts: Bag_Base
+};
+class usm_pack_200rnd_556_bandoliers: Bag_Base
+{
+    class TransportMagazines
     {
-        maximumLoad = 40;
-        class TransportMagazines
+        delete _xx_CUP_200Rnd_TE4_Red_Tracer_556x45_M249;
+        class _xx_rhs_200rnd_556x45_M_SAW
         {
-            delete _xx_CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M;
-            class _xx_rhs_200rnd_556x45_M_SAW
-            {
-                magazine = "rhs_200rnd_556x45_M_SAW";
-                count = 1;
-            };
+            magazine = "rhs_200rnd_556x45_M_SAW";
+            count = 3;
         };
     };
-    class usm_pack_abag_m60: Bag_Base
+};
+class usm_pack_762x51_ammobelts: Bag_Base
+{
+    displayName = "Linked, NATO 7.62x51mm, 100 rnd";
+    maximumLoad = 60;
+    class TransportMagazines
     {
+        delete _xx_CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M;
+        class _xx_hlc_100Rnd_762x51_M_M60E4
+        {
+            magazine = "hlc_100Rnd_762x51_M_M60E4";
+            count = 1;
+        };
     };
+};
