7e65dc44e20a47382cb0480609e1e86bb32e7a44
boberro <m.cyraniak@gmail.com>
2019-08-19 12:07:24 +0200

    @rof_addons folder

---
+++ @rof_addons/mod.cpp
@@ -0,0 +1,7 @@
+name = "Republic of Folk Addons";
+picture = "rof.paa";
+hidePicture = "false";
+hideName = "false";
+actionName = "Website";
+action = "http://republicoffolk.org/forum/";
+description = "Republic of Folk Addons";
