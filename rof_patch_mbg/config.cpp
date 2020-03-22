3c3ab973b307f750f7691876140958cd98f0b558
boberro <m.cyraniak@gmail.com>
2020-03-22 19:52:13 +0100

    rof_patch_mbg - rhs 20rnd magazine

--- rof_patch_mbg/config.cpp
+++ rof_patch_mbg/config.cpp
@@ -4,40 +4,44 @@ class CfgPatches
 {
     class rof_patch_mbg
     {
-        name = "RoF - MBG M16 Patch";
+        name = "Republic of Folk - MBG M16 Patch";
         units[] = {};
         requiredVersion = 1.88;
         requiredAddons[] = {"cba_main", "mbg_m16a2", "rhsusf_c_weapons"};
-        author = "ROF";
-        authors[] = {"ROF"};
-        version = "0.0.1";
-        versionStr = "0.0.1";
-        versionAr[] = {0,0,1};
+        author = "Republic of Folk";
+        authors[] = {"Republic of Folk"};
+        version = "0.0.2";
+        versionStr = "0.0.2";
+        versionAr[] = {0,0,2};
     };
 };
 
 class CfgMagazines
 {
-    class mbg_mag_20Rnd_556x45_M855A1_Stanag;
-    class rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Green: mbg_mag_20Rnd_556x45_M855A1_Stanag
+    class rhs_mag_20Rnd_556x45_M855A1_Stanag;
+    class rhs_mag_30Rnd_556x45_M855A1_Stanag;
+    class mbg_mag_20Rnd_556x45_M855A1_Stanag : rhs_mag_30Rnd_556x45_M855A1_Stanag {
+        scope = 1;
+    };
+    class rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Green: rhs_mag_20Rnd_556x45_M855A1_Stanag
     {
-        author = "Mondkalb / Republic of Folk";
+        author = "Republic of Folk";
         displayName = "5.56mm M885A1 20Rnd Mag (Tracer Green)";
         picture = "\rof_addons\rof_patch_mbg\data\ui\picture_mbg_stanag_mag_20rnd_green";
         ammo="rhs_ammo_556x45_M855A1_Ball_Green";
         tracersEvery=1;
     };
-    class rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Red: mbg_mag_20Rnd_556x45_M855A1_Stanag
+    class rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Red: rhs_mag_20Rnd_556x45_M855A1_Stanag
     {
-        author = "Mondkalb / Republic of Folk";
+        author = "Republic of Folk";
         displayName = "5.56mm M885A1 20Rnd Mag (Tracer Red)";
         picture = "\rof_addons\rof_patch_mbg\data\ui\picture_mbg_stanag_mag_20rnd_red";
         ammo="rhs_ammo_556x45_M855A1_Ball_Red";
         tracersEvery=1;
     };
-    class rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Yellow: mbg_mag_20Rnd_556x45_M855A1_Stanag
+    class rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Yellow: rhs_mag_20Rnd_556x45_M855A1_Stanag
     {
-        author = "Mondkalb / Republic of Folk";
+        author = "Republic of Folk";
         displayName = "5.56mm M885A1 20Rnd Mag (Tracer Yellow)";
         picture = "\rof_addons\rof_patch_mbg\data\ui\picture_mbg_stanag_mag_20rnd_yellow";
         ammo="rhs_ammo_556x45_M855A1_Ball_Yellow";
@@ -48,6 +52,7 @@ class CfgMagazines
 class CfgMagazineWells {
     class CBA_556x45_STANAG {
         mbg_magazines[] = {
+            "rhs_mag_20Rnd_556x45_M855A1_Stanag",
             "mbg_mag_20Rnd_556x45_M855A1_Stanag",
             "rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Green",
             "rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Red",
@@ -56,6 +61,7 @@ class CfgMagazineWells {
     };
     class CBA_556x45_STANAG_SHORT {
         mbg_magazines[] = {
+            "rhs_mag_20Rnd_556x45_M855A1_Stanag",
             "mbg_mag_20Rnd_556x45_M855A1_Stanag",
             "rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Green",
             "rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Red",
@@ -68,6 +74,7 @@ class CfgWeapons {
     class rhs_weap_m16a4;
     class mbg_m16a1: rhs_weap_m16a4 {
         magazines[] = {
+            "rhs_mag_20Rnd_556x45_M855A1_Stanag",
             "mbg_mag_20Rnd_556x45_M855A1_Stanag",
             "rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Green",
             "rof_mag_20Rnd_556x45_M855A1_Stanag_Tracer_Red",
