23240182f2d742811f041af90423c85f61ff6935
boberro <m.cyraniak@gmail.com>
2020-04-17 11:30:26 +0200

    rof_hosts

---
+++ rof_hosts/config.cpp
@@ -0,0 +1,22 @@
+class CfgPatches
+{
+    class rof_hosts
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.32;
+        requiredAddons[] = {
+            "tmf_adminmenu"
+        };
+        name = "Republic of Folk admin whitelist";
+        author = "Republic of Folk";
+        url = "";
+    };
+};
+
+class tmf_adminmenu_authorized_players {
+// class PlayerName {
+//     uid = "1234567890";
+// };
+#include "_ids.hpp"
+};
