934490244dd685f43f419481f21ab47c8d48d108
boberro <m.cyraniak@gmail.com>
2020-11-11 16:02:45 +0100

    rof_factions: added another variant of desert wiesel

--- rof_factions/CfgVehicles/f_nyx_desert.hpp
+++ rof_factions/CfgVehicles/f_nyx_desert.hpp
@@ -6,7 +6,13 @@ class LT_01_base_F: Tank_F {
             author = "Anthrax";
             textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_scout.paa","A3\Armor_F\Data\camonet_AAF_Digi_Desert_CO.paa","A3\armor_f\data\cage_sand_CO.paa"};
             factions[] = {"IND_F"};
-        }
+        };
+        class ROF_Indep_Desert_GreenNet {
+            displayName = "AAF Desert (Green Net)";
+            author = "Anthrax";
+            textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_scout.paa","A3\Armor_F\Data\camonet_AAF_Digi_Green_CO.paa","A3\armor_f\data\cage_aaf_co.paa"};
+            factions[] = {"IND_F"};
+        };
     };
 };
 
@@ -14,7 +20,10 @@ class LT_01_AT_base_F: LT_01_base_F {
     class TextureSources: TextureSources {
         class ROF_Indep_Desert : ROF_Indep_Desert {
             textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_at.paa","A3\Armor_F\Data\camonet_AAF_Digi_Desert_CO.paa","A3\armor_f\data\cage_sand_CO.paa"};
-        }
+        };
+        class ROF_Indep_Desert_GreenNet : ROF_Indep_Desert_GreenNet {
+            textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_at.paa","A3\Armor_F\Data\camonet_AAF_Digi_Green_CO.paa","A3\armor_f\data\cage_aaf_co.paa"};
+        };
     };
 };
 // class LT_01_scout_base_F: LT_01_base_F {};
@@ -22,13 +31,19 @@ class LT_01_AA_base_F: LT_01_base_F {
     class TextureSources: TextureSources {
         class ROF_Indep_Desert : ROF_Indep_Desert {
             textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_at.paa","A3\Armor_F\Data\camonet_AAF_Digi_Desert_CO.paa","A3\armor_f\data\cage_sand_CO.paa"};
-        }
+        };
+        class ROF_Indep_Desert_GreenNet : ROF_Indep_Desert_GreenNet {
+            textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_at.paa","A3\Armor_F\Data\camonet_AAF_Digi_Green_CO.paa","A3\armor_f\data\cage_aaf_co.paa"};
+        };
     };
 };
 class LT_01_cannon_base_F: LT_01_base_F {
     class TextureSources: TextureSources {
         class ROF_Indep_Desert : ROF_Indep_Desert {
             textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_cannon.paa","A3\Armor_F\Data\camonet_AAF_Digi_Desert_CO.paa","A3\armor_f\data\cage_sand_CO.paa"};
-        }
+        };
+        class ROF_Indep_Desert_GreenNet : ROF_Indep_Desert_GreenNet {
+            textures[] = {"rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_main.paa","rof_addons\rof_factions\data\textures\LT_01\lt_01_desert_cannon.paa","A3\Armor_F\Data\camonet_AAF_Digi_Green_CO.paa","A3\armor_f\data\cage_aaf_co.paa"};
+        };
     };
 };
