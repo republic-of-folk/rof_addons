d2037e4a0776862a6cb46cf147ab934aa1b082e6
boberro <m.cyraniak@gmail.com>
2020-11-23 22:55:27 +0100

    compatibility_gm: balanced GM AKs with RHS AKs (for AI)

--- compatibility_gm/CfgWeapons.hpp
+++ compatibility_gm/CfgWeapons.hpp
@@ -1,3 +1,6 @@
+class gm_SemiAuto;
+class gm_FullAuto;
+
 class CfgWeapons {
     class gm_cannon_base;
     class gm_autoCannon_base;
@@ -59,15 +62,170 @@ class CfgWeapons {
 
     class gm_ak47_base : gm_rifle_base {
         magazineWell[] = {"AK_762x39"};
+
+        modes[] = {
+            "SemiAuto",
+            "FullAuto",
+            "Single_AI",
+            "single_medium_optics1",
+            "single_far_optics2",
+            "fullauto_medium"
+        };
+
+        class SemiAuto : gm_SemiAuto
+        {
+            aiRateOfFire = 1e-006;
+        };
+        class FullAuto : gm_FullAuto
+        {
+            aiRateOfFire = 1e-006;
+        };
+        class Single_AI: SemiAuto
+        {
+            showToPlayer            = 0;
+            aiBurstTerminable       = 1;
+            dispersion = 0.00159995;
+            burstRangeMax           = 3;
+            minRange                = 200;
+            minRangeProbab          = 0.5;
+            midRange                = 300;
+            midRangeProbab          = 0.7;
+            maxRange                = 400;
+            maxRangeProbab          = 0.3;
+            aiRateOfFire            = 1;
+            aiRateOfFireDispersion  = 2;
+        };
+        class fullauto_medium: FullAuto
+        {
+            showToPlayer            = 0;
+            aiBurstTerminable       = 0;
+            dispersion = 0.00159995;
+            burst                   = 2;
+            burstRangeMax           = 6;
+            minRange                = 2;
+            minRangeProbab          = 0.5;
+            midRange                = 75;
+            midRangeProbab          = 0.7;
+            maxRange                = 150;
+            maxRangeProbab          = 0.05;
+            aiRateOfFire            = 1;
+            aiRateOfFireDistance    = 200;
+            aiRateOfFireDispersion  = 2;
+        };
+        class single_medium_optics1: Single_AI
+        {
+            burstRangeMax           = 3;
+            requiredOpticType       = 1;
+            showToPlayer            = 0;
+            minRange                = 2;
+            minRangeProbab          = 0.2;
+            midRange                = 450;
+            midRangeProbab          = 0.7;
+            maxRange                = 600;
+            maxRangeProbab          = 0.2;
+            aiRateOfFire            = 4;
+            aiRateOfFireDistance    = 600;
+            aiRateOfFireDispersion  = 2;
+        };
+        class single_far_optics2: single_medium_optics1
+        {
+            requiredOpticType       = 2;
+            minRange                = 100;
+            minRangeProbab          = 0.1;
+            midRange                = 450;
+            midRangeProbab          = 0.6;
+            maxRange                = 800;
+            maxRangeProbab          = 0.05;
+            aiRateOfFire            = 5;
+            aiRateOfFireDistance    = 800;
+            aiRateOfFireDispersion  = 3;
+        };
     };
     class gm_rpk_base : gm_ak47_base {
         magazineWell[] = {"AK_762x39"};
+
+        class FullAuto : gm_FullAuto {};
+        class ai_FullAuto_close : FullAuto {
+            aiBurstTerminable = 0;
+            dispersion = 0.00159995;
+        };
     };
     class gm_ak74_base : gm_rifle_base {
         magazineWell[] = {"AK_545x39"};
+
+        modes[] = {
+            "SemiAuto",
+            "FullAuto",
+            "Single_AI",
+            "single_medium_optics1",
+            "single_far_optics2",
+            "fullauto_medium"
+        };
+
+        class SemiAuto : gm_SemiAuto {
+            aiRateOfFire = 1e-006;
+        };
+        class FullAuto : gm_FullAuto {
+            aiRateOfFire = 1e-006;
+        };
+
+        class Single_AI: SemiAuto
+        {
+            showToPlayer            = 0;
+            aiBurstTerminable       = 1;
+            dispersion              = 0.00093;
+            burstRangeMax           = 3;
+            minRange                = 200;
+            minRangeProbab          = 0.5;
+            midRange                = 300;
+            midRangeProbab          = 0.7;
+            maxRange                = 400;
+            maxRangeProbab          = 0.3;
+            aiRateOfFire            = 1;
+            aiRateOfFireDispersion  = 2;
+        };
+        class single_medium_optics1: Single_AI
+        {
+            requiredOpticType       = 1;
+            minRange                = 2;
+            minRangeProbab          = 0.2;
+            midRange                = 550;
+            midRangeProbab          = 0.7;
+            maxRange                = 700;
+            maxRangeProbab          = 0.1;
+            aiRateOfFire            = 3;
+            aiRateOfFireDistance    = 600;
+            aiRateOfFireDispersion  = 3;
+        };
+        class single_far_optics2: single_medium_optics1
+        {
+            requiredOpticType = 2;
+        };
+        class fullauto_medium: FullAuto
+        {
+            showToPlayer            = 0;
+            aiBurstTerminable       = 0;
+            dispersion              = 0.00093;
+            burst                   = 2;
+            burstRangeMax           = 5;
+            minRange                = 2;
+            minRangeProbab          = 0.5;
+            midRange                = 75;
+            midRangeProbab          = 0.7;
+            maxRange                = 150;
+            maxRangeProbab          = 0.05;
+            aiRateOfFire            = 1.0;
+            aiRateOfFireDispersion  = 2;
+        };
     };
     class gm_rpk74_base : gm_ak74_base {
         magazineWell[] = {"AK_545x39"};
+
+        class FullAuto : gm_FullAuto {};
+        class ai_FullAuto_close : FullAuto {
+            aiBurstTerminable       = 0;
+            dispersion              = 0.00093;
+        };
     };
     class gm_g3_base: gm_rifle_base {};
     class gm_m16_base: gm_rifle_base {};
