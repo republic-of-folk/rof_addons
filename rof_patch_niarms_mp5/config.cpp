8fb94add5b1b1ec1c2730ad67a39bd4bc2814375
boberro <m.cyraniak@gmail.com>
2021-02-28 22:07:43 +0100

    feat(rof_patch_niarms_mp5): rename H&K to HK

---
+++ rof_patch_niarms_mp5/config.cpp
@@ -0,0 +1,17 @@
+#define _ARMA_
+
+class CfgPatches
+{
+    class rof_patch_mbg
+    {
+        name = "Republic of Folk - NI Arms MP5 Names";
+        units[] = {};
+        requiredVersion = 1.88;
+        requiredAddons[] = {"hlcweapons_mp5"};
+        author = "Republic of Folk";
+        authors[] = {"Republic of Folk"};
+        version = "0.0.1";
+        versionStr = "0.0.1";
+        versionAr[] = {0,0,1};
+    };
+};
