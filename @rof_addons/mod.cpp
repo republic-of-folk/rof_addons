d62e99214a3ea0e7d63a9d586c6d822c169d33da
Moritz Schmidt <fusselwurm@gmail.com>
2021-08-02 22:34:53 +0200

    do use 0, 1 instead of "false", "true" in mod.cpp
    fixes RPT spam, see https://github.com/diwako/diwako_dui/issues/201 for details

--- @rof_addons/mod.cpp
+++ @rof_addons/mod.cpp
@@ -1,7 +1,7 @@
 name = "Republic of Folk Addons";
 picture = "\x\rof\addons\main\rof.paa";
-hidePicture = "false";
-hideName = "false";
+hidePicture = 0;
+hideName = 0;
 actionName = "Website";
 action = "https://republicoffolk.org/";
 description = "Republic of Folk Addons";
