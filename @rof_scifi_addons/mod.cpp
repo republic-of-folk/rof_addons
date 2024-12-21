d455dadb200c327fb00f38d3d1202d66d6574d9c
Mateusz Cyraniak <m.cyraniak@gmail.com>
2024-12-21 14:10:18 +0100

    chore: sog and sci-fi folders

---
+++ @rof_scifi_addons/mod.cpp
@@ -0,0 +1,7 @@
+name = "Republic of Folk Sci-fi Event Addons";
+picture = "\x\rof\addons\zombies\rof-scifi.paa";
+hidePicture = "false";
+hideName = "false";
+actionName = "Website";
+action = "https://republicoffolk.org/";
+description = "Republic of Folk Sci-fi Event Addons";
---
+++ @rof_sog_addons/mod.cpp
@@ -0,0 +1,7 @@
+name = "Republic of Folk SOG Addons";
+picture = "\x\rof\addons\main\rof.paa";
+hidePicture = "false";
+hideName = "false";
+actionName = "Website";
+action = "https://republicoffolk.org/";
+description = "Republic of Folk Addons";
